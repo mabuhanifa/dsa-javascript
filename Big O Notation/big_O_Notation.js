e6b37c2f93f20f0f5c458d5951466072269d5190
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-25 23:36:57 +0600

    sum(arr)

--- Big O Notation/big_O_Notation.js
+++ Big O Notation/big_O_Notation.js
@@ -7,6 +7,14 @@
 // }
 
 function addUpTo(n) {
-  return n * (n + 1) / 2;
+  return (n * (n + 1)) / 2;
 }
 console.log(addUpTo(5));
+
+function sum(arr) {
+  let total = 0;
+  for (let i = 1; i <= arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
