c5d1483a81f54e06305e5c7b15c97bbb8befcd61
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-25 23:16:05 +0600

    alternative

--- Big O Notation/big_O_Notation.js
+++ Big O Notation/big_O_Notation.js
@@ -1,9 +1,12 @@
-function addUpTo(n) {
-  let total = 0;
-  for (let i = 1; i <= n; i++) {
-    total += i;
-  }
-  return total;
-}
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
 
+function addUpTo(n) {
+  return n * (n + 1) / 2;
+}
 console.log(addUpTo(5));
