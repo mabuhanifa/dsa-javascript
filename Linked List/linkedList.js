6fa31f96979590f3bbbda59f4bdb84a8caa3f3df
mabuhanifa <moh.abuhanifa@gmail.com>
2024-02-29 18:38:04 +0600

    pop()

--- Linked List/linkedList.js
+++ Linked List/linkedList.js
@@ -24,6 +24,23 @@ class LinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(4);
