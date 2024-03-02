2a989855e093851188a8f0c4c5c677dd55e56e96
mabuhanifa <moh.abuhanifa@gmail.com>
2024-03-03 00:02:34 +0600

    DoublyLinkedList

---
+++ Double Link List/doublyLinkList.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
