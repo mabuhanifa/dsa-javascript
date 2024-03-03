764b7a3ecafe0023c59db256ffcfa41d42ab0f25
mabuhanifa <moh.abuhanifa@gmail.com>
2024-03-03 14:37:50 +0600

    doublyLinkedList unshift

--- Double Link List/doublyLinkList.js
+++ Double Link List/doublyLinkList.js
@@ -39,4 +39,17 @@ class DoublyLinkedList {
     this.length--;
     return temp;
   }
+  unshift() {
+    const newNode = new Node(value);
+    if (!this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
