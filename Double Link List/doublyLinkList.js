370f9d6f62b67309bafa260a8b4b6541fcca5173
mabuhanifa <moh.abuhanifa@gmail.com>
2024-03-03 13:33:37 +0600

    DoublyLinkedList push

--- Double Link List/doublyLinkList.js
+++ Double Link List/doublyLinkList.js
@@ -7,8 +7,22 @@ class Node {
 }
 class DoublyLinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
